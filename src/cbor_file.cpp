c725397b0e6f7217be3f70198ba36b2c847cc2e0
tianci.xie_UIH-ST <tianci.xie@uih-surgical.com>
2022-04-20 20:25:42 +0800

    change to fwrite

--- src/cbor_file.cpp
+++ src/cbor_file.cpp
@@ -1,14 +1,33 @@
 #include "cbor_file.h"
-#include "compress.h"
-
+/*
 void cbostream::write_to_disk()
 {
     if (is_open() && m_buf.size() > 10000)
     {
         // std::unique_ptr<unsigned char[]> tp(new unsigned char[m_buf.size() + 32]);
         // auto len = cborio::HuffmanCompress(m_buf.data(), m_buf.size(), tp.get());
+        //fprintf(this)
         write(reinterpret_cast<const char *>(m_buf.data()), m_buf.size());
         m_buf.clear();
-        ++cnt;
     }
+}*/
+
+bool recfile::open(const char *filename)
+{
+    mpFile = fopen(filename, "wb");
+    m_open = mpFile == nullptr ? false : true;
+    return m_open;
+}
+
+void recfile::close()
+{
+    if (m_open)
+    {
+        fclose(mpFile);
+        mpFile = nullptr;
+    }
+}
+
+void recfile::write_to_disk(){
+    
 }
